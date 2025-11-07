c84e4d27275ce1a13595a788da2d40e7ec850869
mosesu-web <mosesu@uoc.edu>
2025-11-07 19:55:57 +0100

    Añadidas las funciones split_into_words y split_into_sentences
    Se crean dos funciones de ayuda para dividir una cadena de texto en palabras y en oraciones.  Sirven de ayuda para las funciones del módulo ejb1_x1_main

--- util_package/text_manager.py
+++ util_package/text_manager.py
@@ -67,3 +67,46 @@ def remove_punctuation_marks(cad):
     """ A function that removes punctuation marks from a word or a text.
     """
     return cad.translate(str.maketrans('', '', string.punctuation))
+
+
+def split_into_words(text: str) -> list[str]:
+    """ 
+    Divide un texto en palabras basandose en el caracter " " para la
+    división. Previamente, se eliminan todos los simbolos de puntuacion
+    del texto.
+    Devuelve una lista que contiene todas las palabras encontradas
+    """
+    word: str =''
+    words_list: list[str] = []
+    for character in remove_punctuation_marks(text):
+        if word and (is_space(character) or is_newline(character)):
+            words_list.append(word)
+            word = ''
+        if not is_space(character) and not is_newline(character):
+            word += character
+    if word:  # Si al final del bucle, ha quedado una palabra dentro de word, esta se incluye
+        words_list.append(word)
+    return words_list
+
+
+def split_into_sentences(text: str, no_punctuation: bool = False) -> list[str]:
+    """ 
+    Divide un string en oraciones usando el caracter especial \n para ello.
+    Si el parámetro no_punctuation tiene el valor True, entonces se eliminan
+    todos los simbolos de puntuación antes de dividir el texto.
+    Devuelve una lista que contiene todas las oraciones encontradas.
+    """
+    sentence: str = ''
+    sentences_list: list[str] = []
+    if no_punctuation:
+        text = remove_punctuation_marks(text)
+    for character in text:
+        if is_newline(character):
+            sentences_list.append(sentence)
+            sentence = ''
+        else:
+            sentence += character
+    if sentence:  # Si al final del bucle, ha quedado una oración dentro de sentence, esta se incluye
+        sentences_list.append(sentence)
+
+    return sentences_list
